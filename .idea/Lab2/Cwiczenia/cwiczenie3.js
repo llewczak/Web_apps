91a6e4cf6d71474b63edc6b8e637b1ba4e79f563
llewczak <znanylukas@gmail.com>
2025-11-21 21:28:24 +0100

    make table look better

--- .idea/Lab2/Cwiczenia/cwiczenie3.js
+++ .idea/Lab2/Cwiczenia/cwiczenie3.js
@@ -28,18 +28,26 @@ function renderTable(data) {
     document.getElementById("tableContainer").innerHTML = table;
 }
 
-document.getElementById("filterText").addEventListener("input", function (){
-   const searchText = this.value.toLowerCase();
-   const filtered = allData.filter(item => item.title.toLowerCase().includes(searchText));
-   renderTable(filtered);
-});
+function search(value) {
+    const searchText = value.toLowerCase();
+    const filtered = allData.filter(item => item.title.toLowerCase().includes(searchText));
+    filter(filtered, document.getElementById("filterSelect").value);
+};
+
+function filter(data, value) {
+    const option = value;
+    let sorted = [...data];
+    if (option === "descending") sorted.sort((a, b) => b.title.localeCompare(a.title));
+    if (option === "ascending") sorted.sort((a, b) => a.title.localeCompare(b.title));
+    renderTable(sorted);
+}
 
 document.getElementById("filterSelect").addEventListener("change", function () {
-   const option = this.value;
-   let sorted = [...allData];
-   if (option==="descending") sorted.sort((a, b) => b.title.localeCompare(a.title));
-   if (option==="ascending") sorted.sort((a, b) => a.title.localeCompare(b.title));
-   renderTable(sorted);
+    search(document.getElementById("filterText").value);
+});
+
+document.getElementById("filterText").addEventListener("input", function () {
+    search(this.value);
 });
 
 window.addEventListener("DOMContentLoaded", loadData);
